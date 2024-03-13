03453a7f239e1ab606cec12790d2c9e5c4068fa4
Adesh Bode <121815318+Adeesh-bode@users.noreply.github.com>
2024-03-13 12:43:43 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
-export const  PORT = 5555;
+export const  PORT = 5555; // yaha deployment url dal dena
 
 export const mongoDBURL="mongodb+srv://genesis:mongodbisgod@mernbookstore.vth0q6q.mongodb.net/?retryWrites=true&w=majority";
