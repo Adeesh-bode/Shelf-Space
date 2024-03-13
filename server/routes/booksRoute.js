f8e3d4cce1eaead40da9f1fc1c30136973f12035
Adesh Bode <121815318+Adeesh-bode@users.noreply.github.com>
2024-03-13 12:59:34 -0700

    Update booksRoute.js

--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -8,7 +8,7 @@ const router  = express.Router();
 /// now create its middleware in sever file  and specify default routing prefix  to "/books" and remove /books from here belows...
 
 //route to save a new 
-router.post('/',async(req,res)=>{
+router.post('/',async(req,res)=>{  // yeh jo req hai ye iss case mai post request hai client side se
     try {
         if( !req.body.title || !req.body.author || !req.body.publishYear ){
             return res.status(400).send({ message: "Send all requiredfields : title, author, publishYear"})
