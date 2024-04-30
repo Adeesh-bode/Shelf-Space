9c3548f6ef770c6e4b88290e19a712472ae5b697
adesh.exc <bodead@rknec.edu>
2024-04-29 23:00:45 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ import booksRoute from "./routes/booksRoute.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+    origin: 'https://shelf-space-frontend.vercel.app', 
+    optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions)); 
 
 app.use("/books", booksRoute);
 app.get("/", (req, res) => {
