447dca0d33c67e846f830bbd3a27b734283535d5
adesh.exc <bodead@rknec.edu>
2024-05-22 09:32:28 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 export const  PORT = 5555;
 
-export const mongoDBURL="mongodb+srv://genesis:mongodbisgod@mernbookstore.vth0q6q.mongodb.net/?retryWrites=true&w=majority";
+export const mongoDBURL="mongodb+srv://genesis:mongodbisgod@mernbookstore.vth0q6q.mongodb.net/?retryWrites=true&w=majority"; /// ye deployed link hai mongodb ki
